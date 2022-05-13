68e1ba26579b6b1313b92d48626a54cea288b950
Elwin Stephan <elwin.stephan@gmail.com>
2022-05-14 00:44:21 +0200

    some progress

--- go.mod
+++ go.mod
@@ -7,5 +7,6 @@ require (
 	github.com/hennedo/escpos v0.0.1 // indirect
 	github.com/knq/escpos v0.0.0-20201012084129-81d0344e35fa // indirect
 	github.com/moovweb/gokogiri v0.0.0-20180713195410-a1a828153468 // indirect
+	github.com/mugli/png2escpos v1.0.1 // indirect
 	github.com/qiniu/iconv v1.2.0 // indirect
 )
--- main.go
+++ main.go
@@ -1,9 +1,11 @@
 package main
 
 import (
+	"bytes"
 	"os"
 
 	"github.com/knq/escpos"
+	png2escpos "github.com/mugli/png2escpos/escpos"
 )
 
 func main() {
@@ -16,36 +18,26 @@ func main() {
 	// w := bufio.NewWriter(f)
 	p := escpos.New(f)
 
+	buf := bytes.Buffer{}
+	if err := png2escpos.PrintImage("qr.png", &buf); err != nil {
+		panic(err)
+	}
+
 	p.Init()
-	p.SetSmooth(1)
+	p.SetAlign("center")
+	p.WriteRaw(buf.Bytes())
+
+	// p.SetSmooth(1)
 	p.SetFontSize(2, 3)
-	p.SetFont("A")
-	p.Write("test ")
 	p.SetFont("B")
-	p.Write("test2 ")
-	p.SetFont("C")
-	p.Write("test3 ")
-	p.Formfeed()
+	fonts := []string{"A", "B", "C"}
+	for _, font := range fonts {
+		p.SetFont(font)
+		p.FormfeedN(5)
+		p.Write("Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua.")
+		p.FormfeedN(5)
+	}
 
-	p.SetFont("B")
-	p.SetFontSize(1, 1)
-
-	p.SetEmphasize(1)
-	p.Write("halle")
-	p.Formfeed()
-
-	p.SetUnderline(1)
-	p.SetFontSize(4, 4)
-	p.Write("halle")
-
-	p.SetReverse(1)
-	p.SetFontSize(2, 4)
-	p.Write("halle")
-	p.Formfeed()
-
-	p.SetFont("C")
-	p.SetFontSize(8, 8)
-	p.Write("halle")
 	p.FormfeedN(5)
 
 	p.Cut()
