ba76d5ab3612826a193c3f9e1218360e18fa11f2
Elwin Stephan <elw@zurich.ibm.com>
2022-08-15 10:00:33 +0200

    add http endpoint

--- go.mod
+++ go.mod
@@ -4,13 +4,26 @@ go 1.18
 
 require (
 	github.com/beefsack/go-rate v0.0.0-20220214233405-116f4ca011a0
+	github.com/elwin/x v0.0.0-20220814170730-6c7ad5b4a3f2
 	github.com/go-telegram-bot-api/telegram-bot-api/v5 v5.5.1
 	github.com/knq/escpos v0.0.0-20201012084129-81d0344e35fa
+	github.com/labstack/echo/v4 v4.8.0
 	github.com/urfave/cli/v2 v2.8.1
 )
 
 require (
 	github.com/cpuguy83/go-md2man/v2 v2.0.1 // indirect
+	github.com/golang-jwt/jwt v3.2.2+incompatible // indirect
+	github.com/labstack/gommon v0.3.1 // indirect
+	github.com/mattn/go-colorable v0.1.12 // indirect
+	github.com/mattn/go-isatty v0.0.14 // indirect
 	github.com/russross/blackfriday/v2 v2.1.0 // indirect
+	github.com/valyala/bytebufferpool v1.0.0 // indirect
+	github.com/valyala/fasttemplate v1.2.1 // indirect
 	github.com/xrash/smetrics v0.0.0-20201216005158-039620a65673 // indirect
+	golang.org/x/crypto v0.0.0-20210817164053-32db794688a5 // indirect
+	golang.org/x/net v0.0.0-20211015210444-4f30a5c0130f // indirect
+	golang.org/x/sys v0.0.0-20220114195835-da31bd327af9 // indirect
+	golang.org/x/text v0.3.7 // indirect
+	golang.org/x/time v0.0.0-20201208040808-7e3f01d25324 // indirect
 )
--- main.go
+++ main.go
@@ -1,16 +1,28 @@
 package main
 
 import (
+	"context"
+	"crypto/subtle"
 	"fmt"
 	"log"
+	"net/http"
 	"os"
+	"os/signal"
+	"strings"
+	"sync"
+	"syscall"
 	"time"
 
 	"github.com/beefsack/go-rate"
+	syncMap "github.com/elwin/x/sync"
 	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
 	"github.com/knq/escpos"
+	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
 	"github.com/urfave/cli/v2"
 )
+
+var authorizedUsers = syncMap.NewMap[string, string]()
 
 func main() {
 	var (
@@ -20,7 +32,7 @@ func main() {
 			Name:  "printer",
 			Usage: "Start printer server",
 			Action: func(c *cli.Context) error {
-				return run(telegramToken, devicePath)
+				return run(c.Context, telegramToken, devicePath)
 			},
 			Flags: []cli.Flag{
 				&cli.StringFlag{
@@ -32,6 +44,7 @@ func main() {
 					Name:        "device_path",
 					Required:    true,
 					Destination: &devicePath,
+					DefaultText: "/dev/usb/lp0",
 				},
 			},
 		}
@@ -40,10 +53,12 @@ func main() {
 	if err := app.Run(os.Args); err != nil {
 		log.Fatal(err)
 	}
+
 }
 
 type printer struct {
-	path string
+	path  string
+	mutex sync.Mutex
 }
 
 func newPrinter(devicePath string) *printer {
@@ -51,7 +66,10 @@ func newPrinter(devicePath string) *printer {
 }
 
 func (p *printer) print(printF func(e *escpos.Escpos) error) error {
-	f, err := os.OpenFile("/dev/usb/lp0", os.O_RDWR, 0)
+	p.mutex.Lock()
+	defer p.mutex.Unlock()
+
+	f, err := os.OpenFile(p.path, os.O_RDWR, 0)
 	if err != nil {
 		return err
 	}
@@ -60,16 +78,62 @@ func (p *printer) print(printF func(e *escpos.Escpos) error) error {
 	return printF(escpos.New(f))
 }
 
-func run(telegramToken, devicePath string) error {
-	printer := newPrinter(devicePath)
+func (p *printer) printText(t time.Time, username, body string) error {
+	fmt.Println(t.String(), username, body)
+
+	return p.print(func(e *escpos.Escpos) error {
+		e.Init()
+		e.FormfeedN(2)
+		e.Write(fmt.Sprintf("Message from: %s\n", username))
+		e.Write(fmt.Sprintf("Received at: %s\n", t.Format("2. January 2006, 15:04")))
+		e.FormfeedN(2)
+		// e.SetAlign("center")
+		e.Write(body)
+		e.FormfeedN(3)
+		e.Cut()
+		e.End()
+
+		return nil
+	})
+}
+
+func run(ctx context.Context, telegramToken, devicePath string) error {
+	p := newPrinter(devicePath)
 
 	// Just a quick connection check
-	if err := printer.print(func(e *escpos.Escpos) error {
+	if err := p.print(func(e *escpos.Escpos) error {
 		return nil
 	}); err != nil {
 		return err
 	}
 
+	authorizedUsers.Set("elwin", "1243")
+	authorizedUsers.Set("pablo", "1234")
+
+	shutdownWebserver := runWebserver(p)
+
+	shutdownTelegram := runTelegram(telegramToken, p)
+
+	c := make(chan os.Signal, 1)
+	signal.Notify(c, syscall.SIGTERM)
+	<-c
+
+	fmt.Println("Shutting down...")
+	ctx, cancel := context.WithTimeout(ctx, 3*time.Second)
+	defer cancel()
+
+	if err := shutdownWebserver(ctx); err != nil {
+		return err
+	}
+
+	if err := shutdownTelegram(ctx); err != nil {
+		log.Fatal(err)
+	}
+
+	return nil
+}
+
+func runTelegram(telegramToken string, p *printer) func(context.Context) error {
 	bot, err := tgbotapi.NewBotAPI(telegramToken)
 	if err != nil {
 		log.Panic(err)
@@ -97,7 +161,13 @@ func run(telegramToken, devicePath string) error {
 		}
 
 		var msg string
-		if len(update.Message.Text) > 1000 {
+
+		body := strings.Split(update.Message.Text, " ")
+		if update.Message.From.ID == 283013051 && len(body) == 3 && body[0] == "/register" {
+			username, password := body[1], body[2]
+			authorizedUsers.Set(username, password)
+			msg = fmt.Sprintf("Cool, registered %s", username)
+		} else if len(update.Message.Text) > 1000 {
 			msg = "Sorry, your message is a bit too long. Please try to limit yourself a bit."
 		} else if ok, _ := userRateLimit[update.Message.From.ID].Try(); !ok {
 			msg = fmt.Sprintf(
@@ -109,20 +179,7 @@ func run(telegramToken, devicePath string) error {
 				"Sorry, there have been too many messages lately. " +
 					"Please wait a bit before sending the next one.",
 			)
-		} else if err := printer.print(func(e *escpos.Escpos) error {
-			e.Init()
-			e.FormfeedN(2)
-			e.Write(fmt.Sprintf("Message from: %s\n", update.Message.From.UserName))
-			e.Write(fmt.Sprintf("Received at: %s\n", update.Message.Time().Format("2. January 2006, 15:04")))
-			e.FormfeedN(2)
-			e.SetAlign("center")
-			e.Write(update.Message.Text)
-			e.FormfeedN(3)
-			e.Cut()
-			e.End()
-
-			return nil
-		}); err != nil {
+		} else if err := p.printText(update.Message.Time(), update.Message.From.UserName, update.Message.Text); err != nil {
 			log.Println(err.Error())
 			msg = "Huh, something went wrong. Try pinging Elwin, maybe he knows what's up?"
 		} else {
@@ -135,5 +192,55 @@ func run(telegramToken, devicePath string) error {
 		}
 	}
 
-	return nil
+	return func(ctx context.Context) error {
+		bot.StopReceivingUpdates()
+
+		return nil
+	}
+}
+
+func runWebserver(p *printer) func(context.Context) error {
+	e := echo.New()
+	e.Use(middleware.BasicAuth(func(username string, password string, c echo.Context) (bool, error) {
+		return true, nil
+
+		expectedPassword := authorizedUsers.Get(username)
+		if !expectedPassword.Exists() {
+			return false, nil
+		}
+
+		return subtle.ConstantTimeCompare([]byte(expectedPassword.Get()), []byte(password)) == 1, nil
+	}))
+
+	e.POST("/print/text", func(c echo.Context) error {
+		username, _, _ := c.Request().BasicAuth()
+
+		params := struct {
+			Description string `query:"description" header:"description" form:"description" json:"description" xml:"description"`
+		}{}
+
+		if err := c.Bind(&params); err != nil {
+			return err
+		}
+
+		if params.Description == "" {
+			return echo.NewHTTPError(http.StatusBadRequest, "Please provide the description value.")
+		}
+
+		if err := p.printText(time.Now(), username, params.Description); err != nil {
+			return echo.NewHTTPError(http.StatusInternalServerError, err)
+		}
+
+		return c.String(http.StatusOK, "Message successfully printed.")
+	})
+
+	go func() {
+		if err := e.Start(":3030"); err != nil {
+			log.Fatal(err)
+		}
+	}()
+
+	return func(ctx context.Context) error {
+		return e.Shutdown(ctx)
+	}
 }
